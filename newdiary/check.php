64d5533f06dbf8704b32804cfd2661b0e08e4ade
MacUser <fea2gg@gmail.com>
2017-05-27 22:29:43 +0800

    完成？

--- newdiary/check.php
+++ newdiary/check.php
@@ -4,31 +4,31 @@ require('../dbconnect.php');
 $_SESSION['login_member_id']=1;
 
 if (!isset($_SESSION['Diary'])) {
-  header('Location:newdiary.php');
-  exit();
+    header('Location:newdiary.php');
+    exit();
 }
 
 //登録ボタンが押された時
 if (!empty($_POST)) {
   // $diary_id = $_SESSION['Diary']['diary_id'];
   // $user_id = $_SESSION['Diary']['user_id'];
-  $title = $_SESSION['Diary']['title'];
-  $contents = $_SESSION['Diary']['contents'];
+    $title = $_SESSION['Diary']['title'];
+    $contents = $_SESSION['Diary']['contents'];
 
     try{
-      $sql = 'INSERT INTO `Diary` SET `diary_id`=?,`user_id`=?,`title`=?,`contents`=?, `created`=NOW()';
-      $data = array($diary_id,$_SESSION['login_member_id'],$title,$contents);
-      $stmt = $dbh->prepare($sql);
-      $stmt->execute($data);
+        $sql = 'INSERT INTO `Diary` SET `diary_id`=?,`user_id`=?,`title`=?,`contents`=?, `created`=NOW()';
+        $data = array($diary_id,$_SESSION['login_member_id'],$title,$contents);
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute($data);
 
-      unset($_SESSION['Diary']);
+        unset($_SESSION['Diary']);
 
-      header('Location: thanks.php');
-      exit();
+        header('Location: thanks.php');
+        exit();
 
     } catch(PDOExceptopn $e){
-      echo 'SQL文実行時エラー:' . $e->getMessage();
-      exit();
+    echo 'SQL文実行時エラー:' . $e->getMessage();
+    exit();
     }
 }
 ?>
